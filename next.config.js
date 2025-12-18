0eba8545b3aea709fa52046a247deb0952b5da67
RicadGobul <wiliamricad0@gmail.com>
2025-12-18 18:30:23 +0700

    Fix build: exclude examples folder and remove invalid next config

--- next.config.js
+++ next.config.js
@@ -10,9 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    allowedDevOrigins: ['preview-chat-a75b88c3-2920-4c75-aac7-7533a927ed04.space.z.ai'],
-  },
 }
 
 module.exports = nextConfig
